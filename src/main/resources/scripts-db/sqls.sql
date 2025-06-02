ed473714fce4816a1ab233642c0c9105fe0e4108
Álvaro Esteves <>
2025-06-02 00:01:26 -0300

    update url

--- src/main/resources/scripts-db/sqls.sql
+++ src/main/resources/scripts-db/sqls.sql
@@ -1,8 +1,8 @@
 SELECT * FROM bolt_energy_db.ralie_usina_csv_import;
-
+SELECT count(*) FROM bolt_energy_db.ralie_usina_csv_import;
 select cod_ceg, nom_Empreendimento, mda_potencia_outorgada_kw FROM bolt_energy_db.ralie_usina_csv_import;
 
-SELECT * FROM bolt_energy_db.ralie_usina_empresa_potencia_gerada;
+SELECT * FROM bolt_energy_db.ralie_usina_empresa_potencia_gerada order by potencia desc;
 
 SELECT sum(potencia) FROM bolt_energy_db.ralie_usina_empresa_potencia_gerada;
 
